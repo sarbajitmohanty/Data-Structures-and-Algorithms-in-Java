e3bf4bd4d866fca46543991e25769ac653e74e65
sarbajitmohanty <sarbajit.mohanty02@gmail.com>
2021-05-17 00:46:01 +0530

    Initial Commit

---
+++ Arrays/ArraySearch.java
@@ -0,0 +1,18 @@
+package sarbajit;
+
+import java.util.Scanner;
+
+public class ArraySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] A = {1,2,3,4,5,6};
+        int key = scanner.nextInt();
+        for (int i=0; i<A.length; i++) {
+            if (A[i]==key){
+                System.out.println("Element found at index: "+i);
+                System.exit(0);
+            }
+        }
+        System.out.println("Element Not Found");
+    }
+}
---
+++ Arrays/ArraySumProduct.java
@@ -0,0 +1,14 @@
+package sarbajit;
+
+public class ArraySumProduct {
+
+    public static void main(String[] args){
+        int[] A = {1,2,3,4,5,6};
+        int sum = 0, product = 1;
+        for (int j : A) {
+            sum = sum + j;
+            product = product * j;
+        }
+        System.out.println("Sum: "+sum+"\n"+"Product: "+product);
+    }
+}
